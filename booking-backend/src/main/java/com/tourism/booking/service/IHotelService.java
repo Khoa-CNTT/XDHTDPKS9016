58e21c82017e2ef0f686a4b6caa6840cf8970e19
tranvulam <tranvulam2392k3@gmail.com>
2025-04-12 20:26:15 +0700

    Init SCRUM-31 Api Supplier Management

---
+++ booking-backend/src/main/java/com/tourism/booking/service/IHotelService.java
@@ -0,0 +1,14 @@
+package com.tourism.booking.service;
+
+import com.tourism.booking.model.Hotel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+public interface IHotelService {
+    Page<Hotel> getHotels(Pageable pageable);
+    Optional<Hotel> getHotelById(Long hotelId);
+    Hotel save(Hotel hotel);
+    void deleteHotel(Long hotelId);
+}
